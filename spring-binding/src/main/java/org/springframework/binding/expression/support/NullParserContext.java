0a09290635d81c3b69bc6eb947bc833709509930
Keith Donald <kdonald@vmware.com>
2008-01-18 16:38:02 +0000

    javadoc

--- spring-binding/src/main/java/org/springframework/binding/expression/support/NullParserContext.java
+++ spring-binding/src/main/java/org/springframework/binding/expression/support/NullParserContext.java
@@ -18,8 +18,16 @@ package org.springframework.binding.expression.support;
 import org.springframework.binding.expression.ExpressionVariable;
 import org.springframework.binding.expression.ParserContext;
 
+/**
+ * A null object implementation of ParserContext. Mainly used internally by expression parser implementations when
+ * <code>null</code> is passed in as a parser context value.
+ * @author Keith Donad
+ */
 public final class NullParserContext implements ParserContext {
 
+	/**
+	 * The null parser context object instance; a singleton.
+	 */
 	public static final ParserContext INSTANCE = new NullParserContext();
 
 	private NullParserContext() {
