bc95a48cfa35676b90024498c7dcc342bb354e1b
Keith Donald <kdonald@vmware.com>
2008-03-18 21:44:32 +0000

    polish

--- spring-webflow/src/main/java/org/springframework/webflow/engine/FlowInputMappingException.java
+++ spring-webflow/src/main/java/org/springframework/webflow/engine/FlowInputMappingException.java
@@ -1,16 +1,14 @@
 package org.springframework.webflow.engine;
 
 import org.springframework.binding.mapping.MappingResults;
-import org.springframework.core.style.StylerUtils;
 
 public class FlowInputMappingException extends FlowAttributeMappingException {
 	public FlowInputMappingException(String flowId, MappingResults results) {
-		super(flowId, null, results, "Errors occured during flow input mapping; errors = "
-				+ StylerUtils.style(results.getErrorResults()));
+		super(flowId, null, results, "Errors occured during flow input mapping; errors = " + results.getErrorResults());
 	}
 
 	public FlowInputMappingException(String flowId, String stateId, MappingResults results) {
 		super(flowId, stateId, results, "Errors occured during flow input mapping; errors = "
-				+ StylerUtils.style(results.getErrorResults()));
+				+ results.getErrorResults());
 	}
 }
--- spring-webflow/src/main/java/org/springframework/webflow/engine/FlowOutputMappingException.java
+++ spring-webflow/src/main/java/org/springframework/webflow/engine/FlowOutputMappingException.java
@@ -1,7 +1,6 @@
 package org.springframework.webflow.engine;
 
 import org.springframework.binding.mapping.MappingResults;
-import org.springframework.core.style.StylerUtils;
 
 public class FlowOutputMappingException extends FlowAttributeMappingException {
 	public FlowOutputMappingException(String flowId, MappingResults results) {
@@ -10,6 +9,6 @@ public class FlowOutputMappingException extends FlowAttributeMappingException {
 
 	public FlowOutputMappingException(String flowId, String stateId, MappingResults results) {
 		super(flowId, stateId, results, "Errors occured during flow output mapping; errors = "
-				+ StylerUtils.style(results.getErrorResults()));
+				+ results.getErrorResults());
 	}
 }
