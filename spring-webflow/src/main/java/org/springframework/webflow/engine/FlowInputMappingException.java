7fcb7bc2d0b420ce03e83c67d4e0a581780aa0f8
Keith Donald <kdonald@vmware.com>
2008-03-27 11:01:33 +0000

    polish

--- spring-webflow/src/main/java/org/springframework/webflow/engine/FlowInputMappingException.java
+++ spring-webflow/src/main/java/org/springframework/webflow/engine/FlowInputMappingException.java
@@ -4,11 +4,12 @@ import org.springframework.binding.mapping.MappingResults;
 
 public class FlowInputMappingException extends FlowAttributeMappingException {
 	public FlowInputMappingException(String flowId, MappingResults results) {
-		super(flowId, null, results, "Errors occured during flow input mapping; errors = " + results.getErrorResults());
+		super(flowId, null, results, "Errors occurred during input mapping on startup of the '" + flowId
+				+ "' flow; errors = " + results.getErrorResults());
 	}
 
 	public FlowInputMappingException(String flowId, String stateId, MappingResults results) {
-		super(flowId, stateId, results, "Errors occured during flow input mapping; errors = "
-				+ results.getErrorResults());
+		super(flowId, stateId, results, "Errors occurred during input mapping in state '" + stateId + "' of flow '"
+				+ flowId + "'; errors = " + results.getErrorResults());
 	}
 }
--- spring-webflow/src/main/java/org/springframework/webflow/engine/FlowOutputMappingException.java
+++ spring-webflow/src/main/java/org/springframework/webflow/engine/FlowOutputMappingException.java
@@ -4,11 +4,12 @@ import org.springframework.binding.mapping.MappingResults;
 
 public class FlowOutputMappingException extends FlowAttributeMappingException {
 	public FlowOutputMappingException(String flowId, MappingResults results) {
-		this(flowId, null, results);
+		super(flowId, null, results, "Errors occurred during output mapping on ending of the '" + flowId
+				+ "' flow; errors = " + results.getErrorResults());
 	}
 
 	public FlowOutputMappingException(String flowId, String stateId, MappingResults results) {
-		super(flowId, stateId, results, "Errors occured during flow output mapping; errors = "
-				+ results.getErrorResults());
+		super(flowId, stateId, results, "Errors occurred during output mapping in state '" + stateId + "' of flow '"
+				+ flowId + "'; errors = " + results.getErrorResults());
 	}
 }
