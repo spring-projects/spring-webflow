4bc25649e9d6a541960574b652983e82f2799c4a
Scott Andrews <sandrews@vmware.com>
2008-04-01 23:50:28 +0000

    updating package name that moved

--- spring-webflow-samples/booking-mvc/src/main/java/org/springframework/webflow/samples/booking/BookingFlowHandler.java
+++ spring-webflow-samples/booking-mvc/src/main/java/org/springframework/webflow/samples/booking/BookingFlowHandler.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.webflow.core.collection.AttributeMap;
-import org.springframework.webflow.mvc.AbstractFlowHandler;
+import org.springframework.webflow.mvc.servlet.AbstractFlowHandler;
 
 public class BookingFlowHandler extends AbstractFlowHandler {
     public ModelAndView handleExecutionOutcome(String outcome, AttributeMap output, HttpServletRequest request,
