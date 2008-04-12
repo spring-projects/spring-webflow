8bcb3b4ab259559bc51315c5f10c8e05264ee4e0
Keith Donald <kdonald@vmware.com>
2008-04-12 12:11:15 +0000

    Incomplete - task 1: Getting started with your first Spring web application development project

--- spring-webflow-samples/booking-mvc/src/main/java/org/springframework/webflow/samples/booking/BookingFlowHandler.java
+++ spring-webflow-samples/booking-mvc/src/main/java/org/springframework/webflow/samples/booking/BookingFlowHandler.java
@@ -3,6 +3,8 @@ package org.springframework.webflow.samples.booking;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.webflow.core.FlowException;
 import org.springframework.webflow.core.collection.AttributeMap;
 import org.springframework.webflow.mvc.servlet.AbstractFlowHandler;
 
@@ -11,4 +13,10 @@ public class BookingFlowHandler extends AbstractFlowHandler {
 	    HttpServletResponse response) {
 	return "hotels/index";
     }
+
+    @Override
+    public ModelAndView handleException(FlowException e, HttpServletRequest request, HttpServletResponse response) {
+	return new ModelAndView("redirect:/spring/hotels/index");
+    }
+
 }
