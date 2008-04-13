abb3503cc9124c46491864dc4caaccb7c4cb046a
Keith Donald <kdonald@vmware.com>
2008-04-13 05:43:51 +0000

    copyright / javadoc

--- spring-binding/src/main/java/org/springframework/binding/format/registry/DefaultFormatterRegistry.java
+++ spring-binding/src/main/java/org/springframework/binding/format/registry/DefaultFormatterRegistry.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.binding.format.registry;
 
 import java.math.BigDecimal;
@@ -17,10 +32,16 @@ public class DefaultFormatterRegistry extends GenericFormatterRegistry {
 	 */
 	private static DefaultFormatterRegistry SHARED_INSTANCE;
 
+	/**
+	 * Creates a new formatter registry.
+	 */
 	public DefaultFormatterRegistry() {
 		registerDefaultFormatters();
 	}
 
+	/**
+	 * Registers the default formatters. Subclasses may override.
+	 */
 	protected void registerDefaultFormatters() {
 		registerFormatter(Integer.class, new NumberFormatter(Integer.class));
 		registerFormatter(Long.class, new NumberFormatter(Long.class));
