0949ccbb96b0e4196ac32fd8a351959bc9d126ce
Jeremy Grelle <jgrelle@vmware.com>
2008-04-23 19:27:38 +0000

    SWF-646 - Improve the SelectionTrackingActionListener to work with any iterator type of component that works with a DataModel.

--- spring-faces/src/main/java/org/springframework/faces/model/SelectionTrackingActionListener.java
+++ spring-faces/src/main/java/org/springframework/faces/model/SelectionTrackingActionListener.java
@@ -15,6 +15,8 @@
  */
 package org.springframework.faces.model;
 
+import java.lang.reflect.Method;
+
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIData;
 import javax.faces.component.UIViewRoot;
@@ -25,14 +27,16 @@ import javax.faces.event.ActionListener;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.faces.webflow.FlowActionListener;
+import org.springframework.util.ReflectionUtils;
 
 /**
  * Custom {@link ActionListener} that inspects the {@link UIComponent} that signaled the current {@link ActionEvent} to
- * determine whether it is a child of a {@link UIData} component that uses a {@link SelectionAware} data model
- * implementation. If a containing SelectionAware model is found, the row containing the event-signaling component
- * instance will be selected. This enables convenient access to the selected model state at any time through EL
- * expressions such as #{model.selectedRow.id} without having to rely on the whether or not the current row index is
- * pointing to the desired row as it would need to be to use an expression such as #{model.rowData.id}
+ * determine whether it is a child of any iterator type of component (such as {@link UIData}) that uses a
+ * {@link SelectionAware} data model implementation. If a containing SelectionAware model is found, the row containing
+ * the event-signaling component instance will be selected. This enables convenient access to the selected model state
+ * at any time through EL expressions such as #{model.selectedRow.id} without having to rely on the whether or not the
+ * current row index is pointing to the desired row as it would need to be to use an expression such as
+ * #{model.rowData.id}
  * 
  * @author Jeremy Grelle
  */
@@ -52,22 +56,22 @@ public class SelectionTrackingActionListener implements ActionListener {
 	}
 
 	private void trackSelection(UIComponent component) {
-		// Find parent UIData instance if it exists
-		UIData table = null;
+		// Find parent component with a SelectionAware model if it exists
 		UIComponent currentComponent = component;
 		while (!(currentComponent.getParent() instanceof UIViewRoot)) {
-			if (currentComponent.getParent() instanceof UIData) {
-				table = (UIData) currentComponent.getParent();
-				break;
+			UIComponent parent = currentComponent.getParent();
+			Method valueAccessor = ReflectionUtils.findMethod(parent.getClass(), "getValue");
+			if (valueAccessor != null) {
+				Object value = ReflectionUtils.invokeMethod(valueAccessor, parent);
+				if (value != null && value instanceof SelectionAware) {
+					((SelectionAware) value).setSelected(true);
+					if (logger.isDebugEnabled()) {
+						logger.debug("Row selection has been set on the current SelectionAware data model.");
+					}
+					break;
+				}
 			}
 			currentComponent = currentComponent.getParent();
-		}
-		if (table != null && table.getValue() instanceof SelectionAware) {
-			SelectionAware selectableModel = (SelectionAware) table.getValue();
-			selectableModel.setSelected(true);
-			if (logger.isDebugEnabled()) {
-				logger.debug("Row selection has been set on the current SelectionAware data model.");
-			}
 		}
 	}
 
