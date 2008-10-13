921fe06234f6e609e6662cf674d5639de3684243
Jeremy Grelle <jgrelle@vmware.com>
2008-10-13 15:52:25 +0000

    SWF-906 - Upgrade to Dojo 1.2 - Clean Commit

---
+++ spring-js/src/main/resources/META-INF/dojo/tests.js
@@ -0,0 +1,8 @@
+/*
+	Copyright (c) 2004-2008, The Dojo Foundation All Rights Reserved.
+	Available via Academic Free License >= 2.1 OR the modified BSD license.
+	see: http://dojotoolkit.org/license for details
+*/
+
+
+load("dojo.js");load("tests/runner.js");tests.run();
