ccaf8f5136943795f4a5649b9dc5e6599e4b2b16
Keith Donald <kdonald@vmware.com>
2009-04-16 22:43:29 +0000

    javadoc

--- spring-faces/src/main/java/org/springframework/faces/webflow/FlowSerializedView.java
+++ spring-faces/src/main/java/org/springframework/faces/webflow/FlowSerializedView.java
@@ -4,13 +4,25 @@ import java.io.Serializable;
 
 import org.springframework.core.style.ToStringCreator;
 
+/**
+ * Serialized UIViewRoot stored in view scope associated with a Web Flow View State.
+ * 
+ * @author Jeremy Grelle
+ */
 public class FlowSerializedView implements Serializable {
+
 	private Object treeStructure;
 
 	private Object componentState;
 
 	private String viewId;
 
+	/**
+	 * Creates a new serialized view
+	 * @param viewId the view id
+	 * @param treeStructure the tree structure of the view
+	 * @param componentState the component state
+	 */
 	public FlowSerializedView(String viewId, Object treeStructure, Object componentState) {
 		this.viewId = viewId;
 		this.treeStructure = treeStructure;
