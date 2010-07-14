664acbfb489f57214ba845ca67fd8a57821c5fa4
Rossen Stoyanchev <rstoyanchev@vmware.com>
2010-07-14 14:26:52 +0000

    SWF-1362 Add script to prepare custom dojo build, remove dojo and dijit directories, invoke shrinksafe to minify Spring.js and Spring-Dojo.js during the build

---
+++ build-spring-webflow/dojo-build/README.txt
@@ -0,0 +1,3 @@
+
+This directory contains the custom build of Dojo that distributed with Spring JavaScript. 
+See ../../spring-js/scripts/dojo/README.txt for details.
--- spring-js/src/main/resources/META-INF/spring/Spring.js
+++ spring-js/src/main/resources/META-INF/spring/Spring.js
@@ -13,4 +13,164 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-Spring={};Spring.debug=true;Spring.decorations={};Spring.decorations.applied=false;Spring.initialize=function(){Spring.applyDecorations();Spring.remoting=new Spring.RemotingHandler();};Spring.addDecoration=function(_1){if(!Spring.decorations[_1.elementId]){Spring.decorations[_1.elementId]=[];Spring.decorations[_1.elementId].push(_1);}else{var _2=false;for(var i=0;i<Spring.decorations[_1.elementId].length;i++){var _4=Spring.decorations[_1.elementId][i];if(_4.equals(_1)){if(_4.cleanup!=undefined){_4.cleanup();}Spring.decorations[_1.elementId][i]=_1;_2=true;break;}}if(!_2){Spring.decorations[_1.elementId].push(_1);}}if(Spring.decorations.applied){_1.apply();}};Spring.applyDecorations=function(){if(!Spring.decorations.applied){for(var _5 in Spring.decorations){for(var x=0;x<Spring.decorations[_5].length;x++){Spring.decorations[_5][x].apply();}}Spring.decorations.applied=true;}};Spring.validateAll=function(){var _7=true;for(var _8 in Spring.decorations){for(var x=0;x<Spring.decorations[_8].length;x++){if(Spring.decorations[_8][x].widget&&!Spring.decorations[_8][x].validate()){_7=false;}}}return _7;};Spring.validateRequired=function(){var _a=true;for(var _b in Spring.decorations){for(var x=0;x<Spring.decorations[_b].length;x++){if(Spring.decorations[_b][x].widget&&Spring.decorations[_b][x].isRequired()&&!Spring.decorations[_b][x].validate()){_a=false;}}}return _a;};Spring.AbstractElementDecoration=function(){};Spring.AbstractElementDecoration.prototype={elementId:"",widgetType:"",widgetModule:"",widget:null,widgetAttrs:{},apply:function(){},validate:function(){},isRequired:function(){},equals:function(_d){}};Spring.AbstractValidateAllDecoration=function(){};Spring.AbstractValidateAllDecoration.prototype={event:"",elementId:"",apply:function(){},cleanup:function(){},handleEvent:function(_e){},equals:function(_f){}};Spring.AbstractCommandLinkDecoration=function(){};Spring.AbstractCommandLinkDecoration.prototype={elementId:"",linkHtml:"",apply:function(){},submitFormFromLink:function(_10,_11,_12){},equals:function(_13){}};Spring.AbstractAjaxEventDecoration=function(){};Spring.AbstractAjaxEventDecoration.prototype={event:"",elementId:"",sourceId:"",formId:"",popup:false,params:{},apply:function(){},cleanup:function(){},submit:function(_14){},equals:function(_15){}};Spring.AbstractRemotingHandler=function(){};Spring.AbstractRemotingHandler.prototype={submitForm:function(_16,_17,_18){},getLinkedResource:function(_19,_1a,_1b){},getResource:function(_1c,_1d,_1e){},handleResponse:function(){},handleError:function(){}};
+Spring = {};
+
+Spring.debug = true;
+		
+Spring.decorations = {};
+
+Spring.decorations.applied = false;
+
+Spring.initialize = function(){
+	Spring.applyDecorations();
+	Spring.remoting = new Spring.RemotingHandler();
+};
+
+Spring.addDecoration = function(/*Object*/decoration){
+	//Spring.decorations.push(decoration);
+	
+	if (!Spring.decorations[decoration.elementId]) {
+		Spring.decorations[decoration.elementId] = [];
+		Spring.decorations[decoration.elementId].push(decoration);
+	} else {
+		var replaced = false;
+		for(var i = 0; i<Spring.decorations[decoration.elementId].length; i++) {
+			var existingDecoration = Spring.decorations[decoration.elementId][i];
+			if(existingDecoration.equals(decoration)) {
+				if (existingDecoration.cleanup != undefined) {
+					existingDecoration.cleanup();
+				}
+				Spring.decorations[decoration.elementId][i] = decoration;
+				replaced=true;
+				break;
+			}
+		}
+		if (!replaced) {
+			Spring.decorations[decoration.elementId].push(decoration);
+		}
+	}
+	
+	if(Spring.decorations.applied) {
+		decoration.apply();
+	}
+};
+		
+Spring.applyDecorations = function(){
+	if (!Spring.decorations.applied) {
+		for (var elementId in Spring.decorations) {
+			for (var x = 0; x < Spring.decorations[elementId].length; x++) {
+				Spring.decorations[elementId][x].apply();
+			}
+		}
+		Spring.decorations.applied = true;
+	}
+};
+
+Spring.validateAll = function(){
+	var valid = true;
+	for (var elementId in Spring.decorations) {
+		for (var x = 0; x < Spring.decorations[elementId].length; x++) {
+			if (Spring.decorations[elementId][x].widget && !Spring.decorations[elementId][x].validate()) {
+				valid = false;
+			}
+		}
+	}
+	return valid;
+};
+
+Spring.validateRequired = function(){
+	var valid = true;
+	for (var elementId in Spring.decorations) {
+		for (var x = 0; x < Spring.decorations[elementId].length; x++) {
+			if (Spring.decorations[elementId][x].widget && Spring.decorations[elementId][x].isRequired() && 
+			!Spring.decorations[elementId][x].validate()) {
+				valid = false;
+			}
+		}
+	}
+	return valid;
+};
+
+Spring.AbstractElementDecoration = function(){};
+
+Spring.AbstractElementDecoration.prototype = {
+	
+	elementId : "",
+	widgetType : "",
+	widgetModule : "",
+	widget : null,
+	widgetAttrs : {},
+	
+	apply : function(){},
+	
+	validate : function(){},
+	
+	isRequired : function(){},
+	
+	equals : function(/*Object*/other){}
+};
+
+Spring.AbstractValidateAllDecoration = function(){};
+
+Spring.AbstractValidateAllDecoration.prototype = {
+	
+	event : "",
+	elementId : "",
+	
+	apply : function() {},
+	
+	cleanup : function(){},
+	
+	handleEvent : function(event){},
+	
+	equals : function(/*Object*/other){}
+};
+
+Spring.AbstractCommandLinkDecoration = function(){};
+
+Spring.AbstractCommandLinkDecoration.prototype = {
+	
+	elementId : "",
+	linkHtml : "",
+	
+	apply : function(){},
+	
+	submitFormFromLink : function(/*String*/ formId, /*String*/ sourceId, /*Array of name,value params*/ params){},
+	
+	equals : function(/*Object*/other){}
+};
+
+Spring.AbstractAjaxEventDecoration = function(){};
+
+Spring.AbstractAjaxEventDecoration.prototype = {
+	
+	event : "",
+	elementId : "",
+	sourceId : "",
+	formId : "",
+	popup : false,
+	params : {},
+	
+	apply : function(){},
+	
+	cleanup : function(){},
+	
+	submit : function(event){},
+	
+	equals : function(/*Object*/other){}
+};
+
+Spring.AbstractRemotingHandler = function(){};
+
+Spring.AbstractRemotingHandler.prototype = {
+
+	submitForm : function(/*String */ sourceId, /*String*/formId, /*Object*/ params){}, 
+	
+	getLinkedResource: function(/*String */ linkId, /*Object*/params, /*boolean*/ modal) {},
+	
+	getResource : function(/*String */ resourceUri, /*Object*/params, /*boolean*/ modal) {},
+	
+	handleResponse : function() {},
+	
+	handleError : function() {}
+};
