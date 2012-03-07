da15ce813831896d04d4b67bd3ff2a280fd2c681
Rossen Stoyanchev <rstoyanchev@vmware.com>
2012-03-07 05:30:57 -0500

    Upgrade sample to PrimeFaces 3.1.1
    Issues: SWF-1523

--- spring-webflow-samples/booking-faces/src/main/java/org/springframework/webflow/samples/booking/HotelLazyDataModel.java
+++ spring-webflow-samples/booking-faces/src/main/java/org/springframework/webflow/samples/booking/HotelLazyDataModel.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
 
 public class HotelLazyDataModel extends LazyDataModel<Hotel> {
 
@@ -24,9 +25,9 @@ public class HotelLazyDataModel extends LazyDataModel<Hotel> {
     }
 
     @Override
-    public List<Hotel> load(int first, int pageSize, String sortField, boolean sortOrder, Map<String, String> filters) {
+    public List<Hotel> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
 	searchCriteria.setCurrentPage(first / pageSize + 1);
-	return bookingService.findHotels(searchCriteria, first, sortField, sortOrder);
+	return bookingService.findHotels(searchCriteria, first, sortField, sortOrder.equals(SortOrder.ASCENDING));
     }
 
     @Override
@@ -40,6 +41,14 @@ public class HotelLazyDataModel extends LazyDataModel<Hotel> {
 
     public void setSelected(Hotel selected) {
 	this.selected = selected;
+    }
+
+    public int getCurrentPage() {
+	return this.searchCriteria.getCurrentPage();
+    }
+
+    public int getPageSize() {
+	return this.searchCriteria.getPageSize();
     }
 
 }
