64ef54a2e76f1fbe8b335847b065fbd547333397
Phillip Webb <pwebb@vmware.com>
2012-11-14 16:00:17 -0800

    Additional FacesConversionService constructor
    Add the inherited constructor allowing a delegate ConversionService
    to be used
    
    Issue: SWF-1562

--- spring-faces/src/main/java/org/springframework/faces/model/converter/FacesConversionService.java
+++ spring-faces/src/main/java/org/springframework/faces/model/converter/FacesConversionService.java
@@ -14,25 +14,29 @@ package org.springframework.faces.model.converter;
 
 import javax.faces.model.DataModel;
 
-import org.springframework.binding.convert.ConversionService;
 import org.springframework.binding.convert.service.DefaultConversionService;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.faces.model.OneSelectionTrackingListDataModel;
 
 /**
- * Convenient {@link ConversionService} implementation for JSF that composes JSF-specific converters with the standard
- * Web Flow converters.
- * 
+ * Convenient {@link org.springframework.binding.convert.ConversionService} implementation for JSF that composes
+ * JSF-specific converters with the standard Web Flow converters.
+ *
  * <p>
  * In addition to the standard Web Flow conversion, this service provide conversion from a list into a
  * {@link OneSelectionTrackingListDataModel} using a "dataModel" alias for the type.
  * </p>
- * 
+ *
  * @author Jeremy Grelle
  */
 public class FacesConversionService extends DefaultConversionService {
 
 	public FacesConversionService() {
 		addFacesConverters();
+	}
+
+	public FacesConversionService(ConversionService delegateConversionService) {
+		super(delegateConversionService);
 	}
 
 	protected void addFacesConverters() {
