eade36e741b4833d7dceed65f693ecc17e11c954
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2017-12-20 13:54:54 -0500

    Serializable MappingResult(s)
    Issue: SWF-1715

--- spring-binding/src/main/java/org/springframework/binding/mapping/results/AbstractMappingResult.java
+++ spring-binding/src/main/java/org/springframework/binding/mapping/results/AbstractMappingResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2008 the original author or authors.
+ * Copyright 2004-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,8 @@ import org.springframework.core.style.ToStringCreator;
  */
 public abstract class AbstractMappingResult implements MappingResult {
 
-	private Mapping mapping;
+	private transient Mapping mapping;
+
 
 	/**
 	 * Creates a new mapping result.
